2d28b00f87d00d970791cea7035d36fd23d730d0
Rey <mhikeking0@gmail.com>
2024-03-31 16:49:16 +0800

    chore: updated bundle files

--- dist/models/core.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TR_LISTS = exports.TR_BOARD = exports.TR_API_TOKEN = exports.TR_API_KEY = void 0;
-exports.TR_API_KEY = "94435abbd4bd7fd4cc071160fcb7b87e";
-exports.TR_API_TOKEN = "ATTAfa551c260fc09fff3338d48ee801904ba91ba723b70baa400b27d5d9db481c625610FED6";
-exports.TR_BOARD = "bDlztwS8";
-exports.TR_LISTS = 'Back Logs,Bugs,In Progress,For QA,Done';
